c36a78ac85bad3cfe8ed50a48591b36593723252
Mips2648 <mips2648@gmail.com>
2023-05-01 16:44:03 +0200

    cleaning

--- core/ajax/worxLandroidS.ajax.php
+++ core/ajax/worxLandroidS.ajax.php
@@ -49,22 +49,6 @@ try {
         $logs = $eqLogic->get_activity_logs();
         log::add('worxLandroidS', 'debug', 'getActivityLogs:' . json_encode($logs));
         ajax::success($logs);
-    } elseif (init('action') == 'getSchedules') {
-        /** @var worxLandroidS */
-        $eqLogic = eqLogic::byId(init('id'));
-        if (!is_object($eqLogic)) {
-            throw new Exception(__('worxLandroidS eqLogic non trouvé : ', __FILE__) . init('id'));
-        }
-
-        ajax::success(json_decode($eqLogic->getConfiguration('schedules'), true));
-    } elseif (init('action') == 'getZone') {
-        /** @var worxLandroidS */
-        $eqLogic = eqLogic::byId(init('id'));
-        if (!is_object($eqLogic)) {
-            throw new Exception(__('worxLandroidS eqLogic non trouvé : ', __FILE__) . init('id'));
-        }
-
-        ajax::success(json_decode($eqLogic->getConfiguration('zone'), true));
     }
 
     throw new Exception(__('Aucune methode correspondante à: ', __FILE__) . init('action'));
